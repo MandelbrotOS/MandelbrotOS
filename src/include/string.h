ab68d747d60da029245434f7a9ca0ac8690986a9
AtieP <nosequiensoy8664@gmail.com>
2021-05-04 15:28:57 -0400

    Doned

--- src/include/string.h
+++ src/include/string.h
@@ -22,5 +22,6 @@ void *memcpy(void *dest, const void *src, size_t n);
 uint64_t str_to_u64(const char *str);
 
 int strncmp(const char *s1, const char *s2, size_t n);
+int memcmp(const void *s1, const void *s2, size_t n);
 
 #endif // !__STRING_H__
