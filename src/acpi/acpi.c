9e89b1c2eaa956fb699cd2d244190d1fe6cf98f9
AtieP <nosequiensoy8664@gmail.com>
2021-05-04 14:09:53 -0400

    4 spaces -> 2 spaces

--- src/acpi/acpi.c
+++ src/acpi/acpi.c
@@ -11,34 +11,34 @@ static struct acpi_rsdt *rsdt;
 static struct acpi_xsdt *xsdt;
 
 void init_acpi(struct stivale2_struct_tag_rsdp_t *rsdp) {
-    struct acpi_rsdp *rsdp_ = (struct acpi_rsdp *) (uintptr_t) rsdp->rsdp;
-    if (rsdp_->revision >= 2) {
-        use_xsdt = true;
-        xsdt = (struct acpi_xsdt *) (uintptr_t) rsdp_->xsdt_address;
-    } else {
-        use_xsdt = false;
-        rsdt = (struct acpi_rsdt *) (uintptr_t) rsdp_->rsdt_address;
-    }
+  struct acpi_rsdp *rsdp_ = (struct acpi_rsdp *) (uintptr_t) rsdp->rsdp;
+  if (rsdp_->revision >= 2) {
+    use_xsdt = true;
+    xsdt = (struct acpi_xsdt *) (uintptr_t) rsdp_->xsdt_address;
+  } else {
+    use_xsdt = false;
+    rsdt = (struct acpi_rsdt *) (uintptr_t) rsdp_->rsdt_address;
+  }
 }
 
 void *acpi_get_table(const char *signature) {
-    size_t entries;
+  size_t entries;
+  if (use_xsdt) {
+      entries = (xsdt->header.length - sizeof(struct acpi_xsdt)) / sizeof(uint64_t);
+  } else {
+      entries = (rsdt->header.length - sizeof(struct acpi_rsdt)) / sizeof(uint32_t);
+  }
+  for (size_t i = 0; i < entries; i++) {
+    struct acpi_sdt *table_header;
     if (use_xsdt) {
-        entries = (xsdt->header.length - sizeof(struct acpi_xsdt)) / sizeof(uint64_t);
+      table_header = (struct acpi_sdt *) (uintptr_t) xsdt->entries[i];
     } else {
-        entries = (rsdt->header.length - sizeof(struct acpi_rsdt)) / sizeof(uint32_t);
+        table_header = (struct acpi_sdt *) (uintptr_t) rsdt->entries[i];
     }
-    for (size_t i = 0; i < entries; i++) {
-        struct acpi_sdt *table_header;
-        if (use_xsdt) {
-            table_header = (struct acpi_sdt *) (uintptr_t) xsdt->entries[i];
-        } else {
-            table_header = (struct acpi_sdt *) (uintptr_t) rsdt->entries[i];
-        }
-        if (!strncmp(table_header->signature, signature, 4)) {
-            return (void *) table_header;
-        }
+    if (!strncmp(table_header->signature, signature, 4)) {
+        return (void *) table_header;
     }
-    printf("acpi: warning: table with signature %s not found\n", signature);
-    return NULL;
+  }
+  printf("acpi: warning: table with signature %s not found\n", signature);
+  return NULL;
 }
--- src/drivers/pci.c
+++ src/drivers/pci.c
@@ -3,37 +3,37 @@
 #include <hw.h>
 
 uint32_t pci_cfg_read_dword(uint8_t bus, uint8_t slot, uint8_t function, uint8_t offset) {
-    uint32_t address = (uint32_t) 0x80000000 | ((uint32_t) bus << 16) | ((uint32_t) slot << 11) | ((uint32_t) function << 8) | ((uint32_t) offset);
-    outd(PCI_CONFIG, address);
-    return ind(PCI_DATA + (offset & 3));
+  uint32_t address = (uint32_t) 0x80000000 | ((uint32_t) bus << 16) | ((uint32_t) slot << 11) | ((uint32_t) function << 8) | ((uint32_t) offset);
+  outd(PCI_CONFIG, address);
+  return ind(PCI_DATA + (offset & 3));
 }
 
 uint16_t pci_cfg_read_word(uint8_t bus, uint8_t slot, uint8_t function, uint8_t offset) {
-    uint32_t address = (uint32_t) 0x80000000 | ((uint32_t) bus << 16) | ((uint32_t) slot << 11) | ((uint32_t) function << 8) | ((uint32_t) offset);
-    outd(PCI_CONFIG, address);
-    return inw(PCI_DATA + (offset & 3));
+  uint32_t address = (uint32_t) 0x80000000 | ((uint32_t) bus << 16) | ((uint32_t) slot << 11) | ((uint32_t) function << 8) | ((uint32_t) offset);
+  outd(PCI_CONFIG, address);
+  return inw(PCI_DATA + (offset & 3));
 }
 
 uint8_t pci_cfg_read_byte(uint8_t bus, uint8_t slot, uint8_t function, uint8_t offset) {
-    uint32_t address = (uint32_t) 0x80000000 | ((uint32_t) bus << 16) | ((uint32_t) slot << 11) | ((uint32_t) function << 8) | ((uint32_t) offset);
-    outd(PCI_CONFIG, address);
-    return inb(PCI_DATA + (offset & 3));
+  uint32_t address = (uint32_t) 0x80000000 | ((uint32_t) bus << 16) | ((uint32_t) slot << 11) | ((uint32_t) function << 8) | ((uint32_t) offset);
+  outd(PCI_CONFIG, address);
+  return inb(PCI_DATA + (offset & 3));
 }
 
 void pci_cfg_write_dword(uint8_t bus, uint8_t slot, uint8_t function, uint8_t offset, uint32_t value) {
-    uint32_t address = (uint32_t) 0x80000000 | ((uint32_t) bus << 16) | ((uint32_t) slot << 11) | ((uint32_t) function << 8) | ((uint32_t) offset);
-    outd(PCI_CONFIG, address);
-    outd(PCI_DATA + (offset & 3), value);
+  uint32_t address = (uint32_t) 0x80000000 | ((uint32_t) bus << 16) | ((uint32_t) slot << 11) | ((uint32_t) function << 8) | ((uint32_t) offset);
+  outd(PCI_CONFIG, address);
+  outd(PCI_DATA + (offset & 3), value);
 }
 
 void pci_cfg_write_word(uint8_t bus, uint8_t slot, uint8_t function, uint8_t offset, uint16_t value) {
-    uint32_t address = (uint32_t) 0x80000000 | ((uint32_t) bus << 16) | ((uint32_t) slot << 11) | ((uint32_t) function << 8) | ((uint32_t) offset);
-    outd(PCI_CONFIG, address);
-    outw(PCI_DATA + (offset & 3), value);
+  uint32_t address = (uint32_t) 0x80000000 | ((uint32_t) bus << 16) | ((uint32_t) slot << 11) | ((uint32_t) function << 8) | ((uint32_t) offset);
+  outd(PCI_CONFIG, address);
+  outw(PCI_DATA + (offset & 3), value);
 }
 
 void pci_cfg_write_byte(uint8_t bus, uint8_t slot, uint8_t function, uint8_t offset, uint8_t value) {
-    uint32_t address = (uint32_t) 0x80000000 | ((uint32_t) bus << 16) | ((uint32_t) slot << 11) | ((uint32_t) function << 8) | ((uint32_t) offset);
-    outd(PCI_CONFIG, address);
-    outb(PCI_DATA + (offset & 3), value);
+  uint32_t address = (uint32_t) 0x80000000 | ((uint32_t) bus << 16) | ((uint32_t) slot << 11) | ((uint32_t) function << 8) | ((uint32_t) offset);
+  outd(PCI_CONFIG, address);
+  outb(PCI_DATA + (offset & 3), value);
 }
